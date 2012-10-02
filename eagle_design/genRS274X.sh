1ea028bce166dc250fe52d9c5378611b1af1363c
Tyler Nichols <nichols.tyler@gmail.com>
2012-10-02 10:17:30 -0600

    more modifications

--- eagle_design/genRS274X.sh
+++ eagle_design/genRS274X.sh
@@ -15,11 +15,11 @@ fi
 for board in $@; do
     outputfile=$(basename $board .brd)
     outputdir="./tmp"
-    
+
     if [ ! -d ${outputdir} ]; then
 	mkdir -p ${outputdir}
     fi
-    
+
     # Create the following layers:
     #  - top copper (cmp)
     #  - bottom copper (sol)
@@ -35,24 +35,42 @@ for board in $@; do
     eagle -X -dGERBER_RS274X -o"${outputdir}/Top Silkscreen.plc" ${board} Dimension tPlace tNames tValues
     eagle -X -dGERBER_RS274X -o"${outputdir}/Bottom Silkscreen.pls" ${board} Dimension bPlace bNames bValues
     eagle -X -dGERBER_RS274X -o"${outputdir}/Board Outline.ger" ${board} Dimension
-    
+
     # create drill files
-    eagle -X -dEXCELLON -o${outputdir}/Drills.drd ${board} Drills Holes
-    rm ${outputdir}/*.drd
-    # get drills used from dri file and create drl rack file
-    # (like running drillcfg.ulp: ${EAGLE} -N- -C'RUN drillcfg.ulp; QUIT;' -o${outputfile} ${board}
-    cat ${outputdir}/Drills.dri | \
-	sed -e 's/ *\(T[0-9][0-9]\) *\([0-9.]*..\).*/\1 \2/' | \
-	grep -e "^T[0-9][0-9]" > ${outputdir}/Drills.drd
-    
+    eagle -X -dEXCELLON -o"${outputdir}/Drills.drd" ${board} Drills Holes
+
     # remove unecessary files
     rm ${outputdir}/*.dri
     rm ${outputdir}/*.gpi
-    
+
+    # create readme
+    read -d '' readme <<EOF
+Layer Stack:
+1 - Top silkscreen                   Top Silkscreen.plc
+2 - Top solder mask                  Top Solder Mask.stc
+3 - Top copper layer                 Top Layer.cmp
+4 - Bottom copper layer              Bottom Layer.sol
+5 - Bottom solder mask               Bottom Solder Mask.sts
+6 - Bottom silkscreen                Bottom Silkscreen.pls
+
+Board Dimensions:                    Board Outline.ger
+Drill File:                          Drill.drd
+
+Note: all layers are viewed from the TOP of the board.
+
+Tyler Nichols
+nichols.tyler@gmail.cm
+801-473-1530 (cell)
+EOF
+    DATE=$(date --reference=${board})
+    printf "Eagle .brd File: ${board}\n" > ${outputdir}/Readme.txt
+    printf "Last Modified: $DATE\n\n" >> ${outputdir}/Readme.txt
+    printf "$readme" >> ${outputdir}/Readme.txt
+
     # zip up output
     cd ${outputdir}
     7z a -tzip ${outputfile}_gerbers.zip *
-    
+
     cp ${outputfile}_gerbers.zip ../
     cd ../
     rm -rf ${outputdir}
